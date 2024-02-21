3d6bb7a018947781a90814895a9d219a2e3ab394
Efe Sah Bulut <sahturgan@gmail.com>
2024-02-21 19:47:56 +0300

    Add project files.

---
+++ Black Jack/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Black_Jack
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.Oyuncu = new System.Windows.Forms.ListBox();
+			this.Casino = new System.Windows.Forms.ListBox();
+			this.lbl2 = new System.Windows.Forms.Label();
+			this.lbl1 = new System.Windows.Forms.Label();
+			this.btnBasla = new System.Windows.Forms.Button();
+			this.btnTekrarOyna = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Oyuncu
+			// 
+			this.Oyuncu.BackColor = System.Drawing.Color.Navy;
+			this.Oyuncu.ForeColor = System.Drawing.Color.White;
+			this.Oyuncu.FormattingEnabled = true;
+			this.Oyuncu.ItemHeight = 25;
+			this.Oyuncu.Location = new System.Drawing.Point(1201, 342);
+			this.Oyuncu.Name = "Oyuncu";
+			this.Oyuncu.Size = new System.Drawing.Size(246, 279);
+			this.Oyuncu.TabIndex = 25;
+			this.Oyuncu.Tag = "";
+			// 
+			// Casino
+			// 
+			this.Casino.BackColor = System.Drawing.Color.Navy;
+			this.Casino.ForeColor = System.Drawing.Color.White;
+			this.Casino.FormattingEnabled = true;
+			this.Casino.ItemHeight = 25;
+			this.Casino.Location = new System.Drawing.Point(1201, 12);
+			this.Casino.Name = "Casino";
+			this.Casino.Size = new System.Drawing.Size(246, 279);
+			this.Casino.TabIndex = 24;
+			this.Casino.Tag = "";
+			// 
+			// lbl2
+			// 
+			this.lbl2.AutoSize = true;
+			this.lbl2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lbl2.Location = new System.Drawing.Point(810, 560);
+			this.lbl2.Name = "lbl2";
+			this.lbl2.Size = new System.Drawing.Size(24, 25);
+			this.lbl2.TabIndex = 23;
+			this.lbl2.Text = "0";
+			// 
+			// lbl1
+			// 
+			this.lbl1.AutoSize = true;
+			this.lbl1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lbl1.Location = new System.Drawing.Point(810, 285);
+			this.lbl1.Name = "lbl1";
+			this.lbl1.Size = new System.Drawing.Size(24, 25);
+			this.lbl1.TabIndex = 22;
+			this.lbl1.Text = "0";
+			// 
+			// btnBasla
+			// 
+			this.btnBasla.Location = new System.Drawing.Point(12, 74);
+			this.btnBasla.Name = "btnBasla";
+			this.btnBasla.Size = new System.Drawing.Size(159, 43);
+			this.btnBasla.TabIndex = 21;
+			this.btnBasla.Text = "Başla";
+			this.btnBasla.UseVisualStyleBackColor = true;
+			this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+			// 
+			// btnTekrarOyna
+			// 
+			this.btnTekrarOyna.Location = new System.Drawing.Point(12, 12);
+			this.btnTekrarOyna.Name = "btnTekrarOyna";
+			this.btnTekrarOyna.Size = new System.Drawing.Size(159, 43);
+			this.btnTekrarOyna.TabIndex = 20;
+			this.btnTekrarOyna.Text = "Tekrar Oyna";
+			this.btnTekrarOyna.UseVisualStyleBackColor = true;
+			this.btnTekrarOyna.Click += new System.EventHandler(this.btnTekrarOyna_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(645, 25);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(349, 241);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 19;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label4.Location = new System.Drawing.Point(290, 878);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(24, 25);
+			this.label4.TabIndex = 18;
+			this.label4.Text = "0";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label3.Location = new System.Drawing.Point(1729, 908);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(60, 25);
+			this.label3.TabIndex = 17;
+			this.label3.Text = "1000";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label2.Location = new System.Drawing.Point(1627, 908);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(96, 25);
+			this.label2.TabIndex = 16;
+			this.label2.Text = "Balance:";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label1.Location = new System.Drawing.Point(218, 878);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(66, 25);
+			this.label1.TabIndex = 15;
+			this.label1.Text = "Total:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Navy;
+			this.ClientSize = new System.Drawing.Size(1924, 1061);
+			this.Controls.Add(this.Oyuncu);
+			this.Controls.Add(this.Casino);
+			this.Controls.Add(this.lbl2);
+			this.Controls.Add(this.lbl1);
+			this.Controls.Add(this.btnBasla);
+			this.Controls.Add(this.btnTekrarOyna);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.Margin = new System.Windows.Forms.Padding(6);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox Oyuncu;
+		private System.Windows.Forms.ListBox Casino;
+		private System.Windows.Forms.Label lbl2;
+		private System.Windows.Forms.Label lbl1;
+		private System.Windows.Forms.Button btnBasla;
+		private System.Windows.Forms.Button btnTekrarOyna;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Black Jack/Form1.cs
@@ -0,0 +1,747 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Black_Jack
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		int balance = 1000, total = 0, oyuncuKartToplam = 0, casinoKartToplam = 0;
+		yuvarlakbutton button1 = new yuvarlakbutton();
+		yuvarlakbutton button2 = new yuvarlakbutton();
+		yuvarlakbutton button3 = new yuvarlakbutton();
+		yuvarlakbutton button4 = new yuvarlakbutton();
+		yuvarlakbutton button5 = new yuvarlakbutton();
+		yuvarlakbutton button6 = new yuvarlakbutton();
+		yuvarlakbutton button7 = new yuvarlakbutton();
+
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			button1.Size = new Size(120, 120);
+			button1.Location = new System.Drawing.Point(400, 800);
+			button1.BackColor = Color.HotPink;
+			button1.FlatAppearance.BorderSize = 0;
+			button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			button1.FlatAppearance.BorderColor = Color.Red;
+			button1.Text = "100";
+			button1.ForeColor = Color.Black;
+			this.Controls.Add(button1);
+			button1.Click += button1_Click;
+
+			button2.Size = new Size(130, 130);
+			button2.Location = new System.Drawing.Point(600, 800);
+			button2.BackColor = Color.Green;
+			button2.FlatAppearance.BorderSize = 0;
+			button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			button2.FlatAppearance.BorderColor = Color.Red;
+			button2.Text = "200";
+			button2.ForeColor = Color.Black;
+			this.Controls.Add(button2);
+			button2.Click += button2_Click;
+
+			button3.Size = new Size(140, 140);
+			button3.Location = new System.Drawing.Point(800, 800);
+			button3.BackColor = Color.Yellow;
+			button3.FlatAppearance.BorderSize = 0;
+			button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			button3.FlatAppearance.BorderColor = Color.Red;
+			button3.Text = "500";
+			button3.ForeColor = Color.Black;
+			this.Controls.Add(button3);
+			button3.Click += button3_Click;
+
+			button4.Size = new Size(150, 150);
+			button4.Location = new System.Drawing.Point(1000, 800);
+			button4.BackColor = Color.Orange;
+			button4.FlatAppearance.BorderSize = 0;
+			button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			button4.FlatAppearance.BorderColor = Color.Red;
+			button4.Text = "1000";
+			button4.ForeColor = Color.Black;
+			this.Controls.Add(button4);
+			button4.Click += button4_Click;
+
+			button5.Size = new Size(160, 160);
+			button5.Location = new System.Drawing.Point(1200, 800);
+			button5.BackColor = Color.Red;
+			button5.FlatAppearance.BorderSize = 0;
+			button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			button5.FlatAppearance.BorderColor = Color.Red;
+			button5.Text = "5000";
+			button5.ForeColor = Color.Black;
+			this.Controls.Add(button5);
+			button5.Click += button5_Click;
+
+
+			button6.Size = new Size(120, 120);
+			button6.Location = new System.Drawing.Point(600, 650);
+			button6.BackColor = Color.HotPink;
+			button6.FlatAppearance.BorderSize = 0;
+			button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			button6.FlatAppearance.BorderColor = Color.Red;
+			button6.Text = "Kart Çek";
+			button6.ForeColor = Color.Black;
+			this.Controls.Add(button6);
+			button6.Click += button6_Click;
+
+
+			button7.Size = new Size(120, 120);
+			button7.Location = new System.Drawing.Point(800, 650);
+			button7.BackColor = Color.HotPink;
+			button7.FlatAppearance.BorderSize = 0;
+			button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			button7.FlatAppearance.BorderColor = Color.Red;
+			button7.Text = "Dur";
+			button7.ForeColor = Color.Black;
+			this.Controls.Add(button7);
+			button7.Click += button7_Click;
+
+
+			button8.Size = new Size(120, 120);
+			button8.Location = new System.Drawing.Point(1000, 650);
+			button8.BackColor = Color.HotPink;
+			button8.FlatAppearance.BorderSize = 0;
+			button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			button8.FlatAppearance.BorderColor = Color.Red;
+			button8.Text = "2X";
+			button8.ForeColor = Color.Black;
+			this.Controls.Add(button8);
+			button8.Click += button8_Click;
+
+			button6.Visible = false;
+			button7.Visible = false;
+			button8.Visible = false;
+		}
+
+		yuvarlakbutton button8 = new yuvarlakbutton();
+
+		public class yuvarlakbutton : Button
+		{
+			protected override void OnResize(EventArgs e)
+			{
+				base.OnResize(e);
+				GraphicsPath gp = new GraphicsPath();
+				gp.AddEllipse(new Rectangle(Point.Empty, this.Size));
+				this.Region = new Region(gp);
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (balance >= 100)
+			{
+				balance -= 100;
+				total += 100;
+			}
+			else
+			{
+				MessageBox.Show("Paranız Yetersiz!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+
+			label3.Text = Convert.ToString(balance);
+			label4.Text = Convert.ToString(total);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (balance >= 200)
+			{
+				balance -= 200;
+				total += 200;
+			}
+			else
+			{
+				MessageBox.Show("Paranız Yetersiz!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			label3.Text = Convert.ToString(balance);
+			label4.Text = Convert.ToString(total);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (balance >= 500)
+			{
+				balance -= 500;
+				total += 500;
+			}
+			else
+			{
+				MessageBox.Show("Paranız Yetersiz!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+
+			label3.Text = Convert.ToString(balance);
+			label4.Text = Convert.ToString(total);
+		}
+		private void button4_Click(object sender, EventArgs e)
+		{
+			if (balance >= 1000)
+			{
+				balance -= 1000;
+				total += 1000;
+			}
+			else
+			{
+				MessageBox.Show("Paranız Yetersiz!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+
+			label3.Text = Convert.ToString(balance);
+			label4.Text = Convert.ToString(total);
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			if (balance >= 5000)
+			{
+				balance -= 5000;
+				total += 5000;
+			}
+			else
+			{
+				MessageBox.Show("Paranız Yetersiz!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			label3.Text = Convert.ToString(balance);
+			label4.Text = Convert.ToString(total);
+		}
+
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			OyuncuListe();
+
+			if (oyuncuKartToplam > 21)
+			{
+
+				MessageBox.Show("Kaybettiniz" + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+			else if (oyuncuKartToplam == 21)
+			{
+				total *= 2;
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Kazandınız" + " " + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+
+		}
+		private void button7_Click(object sender, EventArgs e)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				casinoCalısma();
+
+			}
+
+			paraHesap();
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+
+			for (int i = 0; i < 5; i++)
+			{
+				if (i == 1)
+				{
+					OyuncuListe();
+					button8.Enabled = false;
+				}
+				else if (casinoKartToplam < oyuncuKartToplam)
+				{
+					CasinoListesi();
+				}
+
+			}
+			if (oyuncuKartToplam > 21)
+			{
+
+				MessageBox.Show("Kaybettiniz" + total * 2);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+			else if (oyuncuKartToplam > casinoKartToplam && oyuncuKartToplam <= 21)
+			{
+
+				total *= 3;
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Kazandınız" + " " + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+			else if (oyuncuKartToplam == 21)
+			{
+
+				total *= 3;
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Kazandınız" + " " + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+
+			}
+			else if (casinoKartToplam > oyuncuKartToplam && casinoKartToplam <= 21)
+			{
+
+
+				MessageBox.Show("Kaybettiniz" + total * 2);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+
+			}
+			else if (casinoKartToplam == 21)
+			{
+
+
+				MessageBox.Show("Kaybettiniz" + total * 2);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+
+			}
+			else if (oyuncuKartToplam == casinoKartToplam)
+			{
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Berabere");
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+
+			else if (casinoKartToplam > oyuncuKartToplam && casinoKartToplam > 21)
+			{
+				total *= 3;
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Kazandınız" + " " + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+		}
+
+		private void btnBasla_Click(object sender, EventArgs e)
+		{
+			for (int i = 1; i <= 3; i++)
+			{
+				if (i == 1)
+				{
+
+					CasinoListesi();
+				}
+
+				if (i > 1)
+				{
+					OyuncuListe();
+				}
+
+
+
+
+				btnBasla.Visible = false;
+			}
+			if (oyuncuKartToplam == 21)
+			{
+				total *= 2;
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Kazandınız" + total);
+				total = 0;
+				label4.Text = total.ToString();
+				BtnKaybet();
+			}
+			button6.Visible = true;
+			button7.Visible = true;
+			button8.Visible = true;
+			button1.Visible = false;
+			button2.Visible = false;
+			button3.Visible = false;
+			button4.Visible = false;
+			button5.Visible = false;
+		}
+
+
+		static string RastgeleRenkSec(List<string> RastgeleRenkSec)
+		{
+
+			Random random = new Random();
+			int rastgeleIndex = random.Next(RastgeleRenkSec.Count);
+
+
+			return RastgeleRenkSec[rastgeleIndex];
+		}
+		static string RastgeleKartSec(List<string> BlackJackListe)
+		{
+
+			Random random = new Random();
+			int rastgeleIndex = random.Next(BlackJackListe.Count);
+
+
+			return BlackJackListe[rastgeleIndex];
+		}
+
+		private void btnTekrarOyna_Click(object sender, EventArgs e)
+		{
+			temizle();
+			BtnKaybet();
+		}
+
+		private void temizle()
+		{
+			Casino.Items.Clear();
+			Oyuncu.Items.Clear();
+			lbl1.Text = "0";
+			lbl2.Text = "0";
+			label4.Text = "0";
+			total = 0;
+			btnBasla.Visible = true;
+			balance = 1000;
+			label3.Text = balance.ToString();
+			oyuncuKartToplam = 0;
+			casinoKartToplam = 0;
+		}
+
+		private void temizle2()
+		{
+			Casino.Items.Clear();
+			Oyuncu.Items.Clear();
+			lbl1.Text = "0";
+			lbl2.Text = "0";
+			label4.Text = "0";
+			total = 0;
+			btnBasla.Visible = true;
+			oyuncuKartToplam = 0;
+			casinoKartToplam = 0;
+		}
+
+		private void casinoCalısma()
+		{
+			if (casinoKartToplam < oyuncuKartToplam)
+			{
+				List<string> BlackJackListe = new List<string>
+		{
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"6",
+			"7",
+			"8",
+			"9",
+			"10",
+			"J",
+			"Q",
+			"K",
+			"A"
+		};
+				List<string> Renkler = new List<string>
+		{
+			"Sinek",
+			"Kupa",
+			"Karo",
+			"Maça",
+
+		};
+
+
+				string rastgelekart = RastgeleKartSec(BlackJackListe);
+				string rastgelerenk = RastgeleRenkSec(Renkler);
+
+				Thread.Sleep(100);
+				Casino.Items.Add(rastgelerenk + " " + rastgelekart);
+				if (rastgelekart == "J" || rastgelekart == "Q" || rastgelekart == "K" || rastgelekart == "10")
+				{
+					casinoKartToplam += 10;
+					lbl1.Text = (Convert.ToString(casinoKartToplam));
+
+				}
+				else if (rastgelekart == "A")
+				{
+
+					if (casinoKartToplam <= 10)
+					{
+						casinoKartToplam += 11;
+						lbl1.Text = (Convert.ToString(casinoKartToplam));
+					}
+					else
+					{
+						casinoKartToplam += 1;
+						lbl1.Text = (Convert.ToString(casinoKartToplam));
+
+					}
+
+				}
+				else
+				{
+					casinoKartToplam += Convert.ToInt16(rastgelekart);
+					lbl1.Text = (Convert.ToString(casinoKartToplam));
+				}
+
+			}
+
+		}
+
+		private void paraHesap()
+		{
+			if (oyuncuKartToplam > 21)
+			{
+
+				MessageBox.Show("Kaybettiniz" + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+			else if (oyuncuKartToplam > casinoKartToplam && oyuncuKartToplam <= 21)
+			{
+				total *= 2;
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Kazandınız" + " " + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+			else if (oyuncuKartToplam == 21)
+			{
+
+				total *= 2;
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Kazandınız" + " " + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+
+			}
+			else if (casinoKartToplam > oyuncuKartToplam && casinoKartToplam <= 21)
+			{
+
+
+				MessageBox.Show("Kaybettiniz" + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+
+			}
+			else if (casinoKartToplam == 21)
+			{
+
+
+				MessageBox.Show("Kaybettiniz" + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+
+			}
+			else if (oyuncuKartToplam == casinoKartToplam)
+			{
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Berabere");
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+
+			else if (casinoKartToplam > oyuncuKartToplam && casinoKartToplam > 21)
+			{
+				total *= 2;
+				balance += total;
+				label3.Text = balance.ToString();
+				MessageBox.Show("Kazandınız" + " " + total);
+				total = 0;
+				label4.Text = total.ToString();
+				temizle2();
+				BtnKaybet();
+			}
+		}
+
+		private void CasinoListesi()
+		{
+			List<string> BlackJackListe = new List<string>
+		{
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"6",
+			"7",
+			"8",
+			"9",
+			"10",
+			"J",
+			"Q",
+			"K",
+			"A"
+		};
+			List<string> Renkler = new List<string>
+		{
+			"Sinek",
+			"Kupa",
+			"Karo",
+			"Maça",
+
+		};
+
+			Thread.Sleep(100);
+			string rastgelekart = RastgeleKartSec(BlackJackListe);
+			string rastgelerenk = RastgeleRenkSec(Renkler);
+			Casino.Items.Add(rastgelerenk + " " + rastgelekart);
+
+
+			if (rastgelekart == "J" || rastgelekart == "Q" || rastgelekart == "K" || rastgelekart == "10")
+			{
+				casinoKartToplam += 10;
+				lbl1.Text = (Convert.ToString(casinoKartToplam));
+
+			}
+
+			else if (rastgelekart == "A")
+			{
+
+				if (casinoKartToplam <= 10)
+				{
+					casinoKartToplam += 11;
+					lbl1.Text = (Convert.ToString(casinoKartToplam));
+
+				}
+				else
+				{
+					casinoKartToplam += 1;
+					lbl1.Text = (Convert.ToString(casinoKartToplam));
+
+				}
+
+			}
+			else
+			{
+				casinoKartToplam += Convert.ToInt16(rastgelekart);
+				lbl1.Text = (Convert.ToString(casinoKartToplam));
+
+			}
+		}
+
+		private void OyuncuListe()
+		{
+
+			List<string> BlackJackListe = new List<string>
+		{
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"6",
+			"7",
+			"8",
+			"9",
+			"10",
+			"J",
+			"Q",
+			"K",
+			"A"
+		};
+			List<string> Renkler = new List<string>
+		{
+			"Sinek",
+			"Kupa",
+			"Karo",
+			"Maça",
+
+		};
+
+
+			string rastgelekart = RastgeleKartSec(BlackJackListe);
+			string rastgelerenk = RastgeleRenkSec(Renkler);
+			Oyuncu.Items.Add(rastgelerenk + " " + rastgelekart);
+
+			Thread.Sleep(100);
+			if (rastgelekart == "J" || rastgelekart == "Q" || rastgelekart == "K" || rastgelekart == "10")
+			{
+				oyuncuKartToplam += 10;
+				lbl2.Text = (Convert.ToString(oyuncuKartToplam));
+
+
+			}
+
+			else if (rastgelekart == "A")
+			{
+
+				if (oyuncuKartToplam <= 10)
+				{
+					oyuncuKartToplam += 11;
+					lbl2.Text = (Convert.ToString(oyuncuKartToplam));
+
+				}
+				else
+				{
+					oyuncuKartToplam += 1;
+					lbl2.Text = (Convert.ToString(oyuncuKartToplam));
+
+				}
+
+
+
+			}
+			else
+			{
+				oyuncuKartToplam += Convert.ToInt16(rastgelekart);
+				lbl2.Text = (Convert.ToString(oyuncuKartToplam));
+
+
+			}
+		}
+
+		private void BtnKaybet()
+		{
+			button6.Visible = false;
+			button7.Visible = false;
+			button8.Visible = false;
+			button1.Visible = true;
+			button2.Visible = true;
+			button3.Visible = true;
+			button4.Visible = true;
+			button5.Visible = true;
+			button8.Enabled = true;
+		}
+
+	}
+}
